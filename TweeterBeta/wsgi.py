be77f5fb70626543c88513782479922b2877fca1
Atiqur Rahman <s.atiqurrahman2003@gmail.com>
2025-06-23 22:08:37 +0600

    update

--- TweeterBeta/wsgi.py
+++ TweeterBeta/wsgi.py
@@ -7,13 +7,30 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TweeterBeta.settings')
+
+# application = get_wsgi_application()
+
+
+# app = application
+
+
 import os
 
-from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TweeterBeta.settings")
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TweeterBeta.settings')
-
-application = get_wsgi_application()
+try:
+    from django.core.wsgi import get_wsgi_application
+    application = get_wsgi_application()
+except Exception as e:
+    # This helps debugging on Vercel
+    import sys
+    print("WSGI Error:", e, file=sys.stderr)
+    application = None
 
 
 app = application
